db6b1968944e63a8a63e0c14dc4cf13fc43c21d4
Rob <69918580+bruhmoent@users.noreply.github.com>
2022-10-12 16:51:19 +0200

    Force correct pawn movement

--- movementCheck.h
+++ movementCheck.h
@@ -2,25 +2,30 @@
 #include <string>
 #include "Rook.h"
 #include "BasePiece.h"
-
+using namespace std;
 
 class MovementCheck : public BasePiece
 {
 public:
-	inline void movementCheck2(int piece, int field) {
+	inline void movementCheck2(int piece, int field, int column, int row, int moveC, int moveR) {
 		//cout << "POLE:"<<piece;
+	//	cout << "\nx: . \n" << column;
+		//cout << "\ny: . \n" << row;
+		//static bool correctMove = true;
 		if (field != 20) {
 			cout << "\nThe field is already taken.\n";
 		}
-		//Piece* szachy2[8][8];
-		//int szachy2[9][];
-	/*	int pawn = choosenPiece;
-		if (szachy2[choosenColumn][choosenRow] == Piece(1)) {
-			cout << "hej!";
-		}*/
-		/*if (choosenPiece == pawn)
-			cout << "hej!";*/
+		switch (piece) {
+		case(1):
+			if (moveC > column || moveR > row+1)
+			{
+				cout << "Your move is out of this piece's reach.";
+				exit(123);
+				break;
+			}
+			break;
 
+		}
+		
 	}
-
 };
